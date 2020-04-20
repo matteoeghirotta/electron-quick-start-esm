c572b409b0c5625b6fe328a813e8513dbf5517cb
Matteo Ricci <matteo.ricci@studiomado.it>
2020-04-20 08:49:26 +0200

    first commit

---
+++ renderer.js
@@ -0,0 +1,39 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// No Node.js APIs are available in this process because
+// `nodeIntegration` is turned off. Use `preload.js` to
+// selectively enable features needed in the rendering
+// process.
+
+import * as THREE from "./three.module.js";
+
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+let renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+// document.body.appendChild( renderer.domElement );
+document.getElementById("webgl").append(renderer.domElement);
+
+let geometry = new THREE.BoxGeometry();
+let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+let cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+let animate = function () {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+};
+
+animate();
